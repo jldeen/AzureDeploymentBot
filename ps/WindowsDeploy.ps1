a127f721c52d29780eb84d71bd8d76614d7ce003
jldeen <jessica@jessicadeen.com>
2016-09-13 09:54:04 -0700

    Added positional parameters

--- ps/WindowsDeploy.ps1
+++ ps/WindowsDeploy.ps1
@@ -1,11 +1,20 @@
-﻿$Location="WestUs"
-$RGName="TestDeploy"
-$DepName="TestDeploy"
+﻿#Run the following to sign into your Azure account
+Add-AzureRmAccount
+
+#Positional Parameters
+Param(
+  [string]$Location,
+  [string]$Resource,
+  [string]$Name
+  )
+
+<# $Location="WestUs"
+$RGName="TestDeploy3"
+$DepName="TestDeploy3" #>
 
 $TempFile="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json"
 
-#Add-AzureRmAccount
+New-AzureRmResourceGroup $Resource $Location
 
-New-AzureRmResourceGroup $RGName $Location
+New-AzureRmResourceGroupDeployment -Name $Name -ResourceGroupName $Resource -TemplateUri $TempFile 
 
-New-AzureRmResourceGroupDeployment -Name $DepName -ResourceGroupName $RGName -TemplateUri $TempFile 
