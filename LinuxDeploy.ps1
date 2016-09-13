59bf56d9b5f749cbdf7075876d04b281f3528c70
Jessica Deen <jldeen@users.noreply.github.com>
2016-09-12 20:13:23 -0700

    First commit

---
+++ LinuxDeploy.ps1
@@ -0,0 +1,11 @@
+﻿$Location="WestUs"
+$RGName="TestDeploy"
+$DepName="TestDeploy"
+
+$TempFile="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json"
+
+#Add-AzureRmAccount
+
+New-AzureRmResourceGroup $RGName $Location
+
+New-AzureRmResourceGroupDeployment -Name $DepName -ResourceGroupName $RGName -TemplateUri $TempFile 
